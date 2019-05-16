19e8b8fd764ec7b660a1657a98c5eab95e5b2d08
Jarrett Kong <jarrettkong@me.com>
2019-05-16 10:03:36 -0600

    Add form style

--- src/ReservationForm.js
+++ src/ReservationForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './ReservationForm.css';
 
 export class ReservationForm extends Component {
 	state = {
@@ -26,9 +27,9 @@ export class ReservationForm extends Component {
 
 	render() {
 		return (
-			<form onSubmit={this.handleSubmit}>
+			<form className="ReservationForm" onSubmit={this.handleSubmit}>
 				<input type="text" placeholder="Name" name="name" id="" onChange={this.handleChange} value={this.state.name} />
-				<input type="text" placeholder="Date" name="date" id="" onChange={this.handleChange} value={this.state.date} />
+				<input type="text" placeholder="Date (mm/dd)" name="date" id="" onChange={this.handleChange} value={this.state.date} />
 				<input type="text" placeholder="Time" name="time" id="" onChange={this.handleChange} value={this.state.time} />
 				<input
 					type="number"
@@ -38,7 +39,7 @@ export class ReservationForm extends Component {
 					onChange={this.handleChange}
 					value={this.state.number}
 				/>
-				<input type="submit" value="Make Reservation" />
+				<input className='submit-btn' type="submit" value="Make Reservation" />
 			</form>
 		);
 	}
