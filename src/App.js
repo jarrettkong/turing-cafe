e71e340ce778193cc1d4d7e225f52448a83a2a1c
Jarrett Kong <jarrettkong@me.com>
2019-05-16 09:15:37 -0600

    Get existing reservations

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
+	state = {
+		reservations: []
+	};
 
-        </div>
-        <div className='resy-container'>
-          
-        </div>
-      </div>
-    )
-  }
+	componentDidMount() {
+		this.getExistingReservations();
+	}
+
+	getExistingReservations = async () => {
+		const res = await fetch('http://localhost:3001/api/v1/reservations');
+		const reservations = await res.json();
+		this.setState({ reservations });
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<h1 className="app-title">Turing Cafe Reservations</h1>
+				<div className="resy-form" />
+				<div className="resy-container" />
+			</div>
+		);
+	}
 }
 
 export default App;
