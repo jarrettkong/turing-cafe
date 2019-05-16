331011c8ef8d987437b4f6eb015c29c33a10371c
Jarrett Kong <jarrettkong@me.com>
2019-05-16 09:48:11 -0600

    Add styles for Reservation

--- src/Reservation.js
+++ src/Reservation.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Reservation.css';
 
 const Reservation = props => {
 	const { data, deleteReservation } = props;
 	return (
-		<article>
+		<article className="Reservation">
 			<h2>{data.name}</h2>
 			<p>{data.date}</p>
-			<p>{data.time}</p>
+			<p>{data.time} pm</p>
 			<p>Numer of Guests: {data.number}</p>
 			<button onClick={() => deleteReservation(data.id)}>Cancel</button>
 		</article>
--- src/ReservationContainer.js
+++ src/ReservationContainer.js
@@ -5,7 +5,7 @@ const ReservationContainer = props => {
 	const reservations = props.reservations.map(res => (
 		<Reservation key={res.id} data={res} deleteReservation={props.deleteReservation} />
 	));
-	return <section>{reservations};</section>;
+	return <section>{reservations}</section>;
 };
 
 export default ReservationContainer;
