b94c9249864a8e04fbd1ac5c7c684eb8ef10b79e
Jarrett Kong <jarrettkong@me.com>
2019-05-16 09:57:01 -0600

    Add grid layout

--- src/ReservationContainer.js
+++ src/ReservationContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Reservation from './Reservation';
+import './ReservationContainer.css';
 
 const ReservationContainer = props => {
 	const reservations = props.reservations.map(res => (
 		<Reservation key={res.id} data={res} deleteReservation={props.deleteReservation} />
 	));
-	return <section>{reservations}</section>;
+	return <section className="ReservationContainer">{reservations}</section>;
 };
 
 export default ReservationContainer;
